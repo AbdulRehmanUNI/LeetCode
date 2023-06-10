61b357d136b0592167f81e9f75e7fcca75e0ada1
AbdulRehman56 <211370196@gift.edu.pk>
2023-06-10 12:17:29 +0500

    Create PalindromeNumber.java

---
+++ Problems/#9/PalindromeNumber.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x<0) return false;
+
+        String num=Integer.toString(x);
+        int s=0;
+        int e=num.length()-1;
+
+        while(s<=e){
+            if(num.charAt(s)!=num.charAt(e)){
+               return false;
+            }
+            s++;
+            e--;
+        }
+        return true;
+    }
+}
+
+
+
+// form leetcode sols , beats >99% on runtime
+
+/*class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+}*/
