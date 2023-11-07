e410e3c8766ca753b86c4f639394f0410bb5dc7b
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2023-11-07 23:48:24 +0500

    Update Solution.java

--- Problems/#100/Solution.java
+++ Problems/#100/Solution.java
@@ -16,7 +16,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
          if(p==null && q==null) return true;
-   
+    
        if(p==null || q==null ||p.val!=q.val) return false;
 
     //    if(p.left!=null &&  q.left!=null)
