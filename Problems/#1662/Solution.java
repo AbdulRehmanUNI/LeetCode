18e1aa057efc7a5f69acef676a224e84dce339bd
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2023-12-01 16:21:02 +0500

    Update Solution.java

--- Problems/#1662/Solution.java
+++ Problems/#1662/Solution.java
@@ -9,3 +9,14 @@ class Solution {
         return a.equals(b);
     }
 }
+
+//dec 1 , daily chlng
+
+// class Solution {
+//     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//         for(int i=1; i<word1.length; i++) word1[0]+=word1[i];
+//         for(int i=1; i<word2.length; i++) word2[0]+=word2[i];
+//         //if(word1[0].length()!=word2[0].length()) return false;
+//         return word1[0].equals(word2[0]);
+//     }
+// }
