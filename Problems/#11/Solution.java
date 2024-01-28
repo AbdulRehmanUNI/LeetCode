ca5ac3178577dd9b16704f734216cbb67f2c92b5
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2024-01-28 20:00:10 +0500

    Update Solution.java

--- Problems/#11/Solution.java
+++ Problems/#11/Solution.java
@@ -21,3 +21,19 @@ class Solution {
         return area;
     }
 }
+
+
+//------------------------------- revision ----------------------
+class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            ans = Math.max(ans, Math.min(height[left], height[right]) * (right-left));
+            // if (height[left] < height[right]) left++;
+            // else right--;
+            if ( height[right]<height[left]) right--;
+            else left++;
+        }
+        return ans;
+    }
+}
