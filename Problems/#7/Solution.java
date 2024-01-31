f70311ba8faeee44cf948ab169bfeb638facdc5e
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2024-01-31 20:12:55 +0500

    Update Solution.java

--- Problems/#7/Solution.java
+++ Problems/#7/Solution.java
@@ -10,3 +10,21 @@ class Solution {
     return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int) ans;
   }
 }
+
+
+//--------------------- revision ----------------------//
+
+class Solution {
+  public int reverse(int x) {
+      boolean isNeg= x<0?true:false;
+      if(isNeg) x=-(x);
+      long ans=0;
+      while(x>0){
+          ans*=10;
+          ans+= (x%10);
+          x/=10;
+      }
+      ans= isNeg? -ans: ans;
+      return (ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE)? 0: (int) ans;  
+  }
+}
