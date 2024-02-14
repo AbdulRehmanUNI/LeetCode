cd97687104b8f2c7223b7cf33144cfb67ea70cb1
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2024-02-14 23:24:57 +0500

    .

--- Problems/#2149/Solution.java
+++ Problems/#2149/Solution.java
@@ -23,3 +23,22 @@ class Solution {
         return nums;
     }
 }
+
+//------------- revision------------------
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] pos=new int[nums.length/2];
+        int[] neg=new int[nums.length/2];
+
+        for(int i=0,j=0,k=0; i<nums.length; i++){
+            if(nums[i]<0) neg[j++]=nums[i];
+            else pos[k++]=nums[i]; 
+        }
+        for(int i=0,j=0,k=0; i<nums.length; i+=2){
+            nums[i]=pos[k++];
+            nums[i+1]=neg[j++];
+        }
+    return nums;
+    }
+}
