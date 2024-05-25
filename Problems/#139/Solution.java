f619d194e581f3713e4a99fca526330aa3e1eacb
Abdul Rehman Khan <211370196@gift.edu.pk>
2024-05-25 18:50:46 +0500

    Update Solution.java

--- Problems/#139/Solution.java
+++ Problems/#139/Solution.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // HashSet<String> set = new HashSet<>(wordDict);
-        // int[] dp=new int[s.length];
-
-        // for(int i=0; i<s.length; i++){
-        // for(int j=i; j<s.length; j++){
-
-        // }
-        // } // i want too far ..... then came back
         return dfs(0, s, new HashSet<>(wordDict), new Boolean[s.length()]);
     }
 
@@ -15,11 +7,12 @@ class Solution {
         if (start == s.length())
             return true;
         if (memo[start] != null)
-            return false; // basic memoization
+            return memo[start]; // memoization
 
-        for (int end = start + 1; end < s.length(); end++) {
-            if (set.contains(s.substring(start, end+1)) && dfs(end, s, set, memo))
-                memo[end] = true;
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (set.contains(s.substring(start, end)) && dfs(end, s, set, memo)) {
+                return memo[start] = true;
+            }
         }
         return memo[start] = false;
     }
