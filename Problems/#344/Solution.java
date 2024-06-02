3f95257f0d733341fb181f435a1cf444ce593100
Abdul Rehman Khan <211370196@gift.edu.pk>
2024-06-02 14:28:47 +0500

    add

--- Problems/#344/Solution.java
+++ Problems/#344/Solution.java
@@ -14,4 +14,15 @@ class Solution {
     }
 }
 
+// 2 june 2024, POTD
 
+class Solution {
+    public void reverseString(char[] s) {
+        int start=0, end=s.length-1;
+        while(start<end){
+            char one=s[start];
+            s[start++]=s[end];
+            s[end--]=one;
+        }
+    }
+}
